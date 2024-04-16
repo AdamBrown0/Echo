c91532ffb0e52586cb3a39cc92aaa8a4cb22412d
AdamBrown0 <adam@djb.org.uk>
2024-04-16 01:30:06 +0100

    Created VGA buffer writing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+mod vga;
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> !{
+    println!("Hello World{}", "!");
+    print!("AAAA");
+    print!("BBBB");
+    println!("THe number is {} and answer is {}", 42, 1.0/3.0);
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> !{
+    loop {}
 }
